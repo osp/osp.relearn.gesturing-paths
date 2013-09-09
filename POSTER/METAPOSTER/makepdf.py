087eaa04d900397a7dab4e93f37b3d1cfd84bf42
Loraine <loraine.furter@gmail.com>
2013-09-09 23:28:36 +0200

    New attempts at a Metaposter
    Reorganisation of the files and folders in the new test folder METAPOSTER

---
+++ POSTER/METAPOSTER/makepdf.py
@@ -0,0 +1,7 @@
+import os 
+print " starting make pdf script "
+print "------------------------------------------------------"
+os.system("latex -output-format='pdf' metaposter-line.tex")
+print "Done! :-)"
+
+
